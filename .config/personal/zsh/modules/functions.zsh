7437cd0a1d7811fe0d2a7bac875f0a8eb793ec8f
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2023-11-03 00:56:45 +0330

    Modify updateall function

--- .config/personal/zsh/modules/functions.zsh
+++ .config/personal/zsh/modules/functions.zsh
@@ -4,13 +4,26 @@ lzyadm() {
     cd -
 }
 
+
 updateall() {
-    echo "[BREW]"
-    brew upgrade
-    echo "[PIPX]"
-    pipx upgrade-all
-    echo "[ZIM]"
-    zimfw upgrade && zimfw update
-    echo "[GUP]"
-    gup update
+    local echo_style() {
+        local yellow='\e[33m'
+        local blue='\e[34m'
+        local reset='\e[0m'
+        local bold='\e[1m'
+        echo -e "${bold}${blue}[${yellow}$1${blue}]${reset}"
+    }
+
+    local run() {
+        local header=$(echo $1 | tr '[:lower:]' '[:upper:]')
+        echo_style $header
+        "$@"
+        print "\e[34m${(l:${$(tput cols)}::⎯:)}"
+    }
+
+    run brew upgrade
+    run pipx upgrade-all
+    run zimfw upgrade
+    run zimfw update
+    run gup update
 }
