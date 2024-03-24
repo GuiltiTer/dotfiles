dac9b10a24a5e46ceb3a9bed28e11afdc8cb7a10
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-03-24 21:45:29 +0330

    fixup! Add wezterm configs

--- .config/wezterm/ui.lua
+++ .config/wezterm/ui.lua
@@ -2,38 +2,19 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-  config.color_scheme = "Catppuccin Mocha"
+	config.color_scheme = "Tokyo Night"
 
-  config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
-  config.font_size = 16
+	config.font = wezterm.font("Cascadia Code", { weight = "Medium" })
+	config.font_size = 16.5
 
-  -- config.use_fancy_tab_bar = false
-  -- config.tab_bar_at_bottom = true
-  -- config.hide_tab_bar_if_only_one_tab = true
-  -- config.show_new_tab_button_in_tab_bar = false
-  -- wezterm.on('format-tab-title', function(tab, _, _, _, _, _)
-  --   local padding = " "
-  --
-  --   if tab.is_active then
-  --     padding = "   "
-  --   end
-  --
-  --   return {
-  --     { Text = padding },
-  --     { Text = tostring(tab.tab_index + 1) },
-  --     { Text = padding },
-  --   }
-  -- end
-  -- )
+	config.window_decorations = "RESIZE"
+	config.adjust_window_size_when_changing_font_size = false
+	config.window_padding = { top = 2, left = 2, right = 2, bottom = 0 }
 
-  config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-  config.adjust_window_size_when_changing_font_size = false
-  config.window_padding = { top = 2, left = 2, right = 2, bottom = 0 }
+	config.cursor_blink_rate = 600
+	config.default_cursor_style = "BlinkingBlock"
 
-  config.command_palette_bg_color = "#181826"
-  config.command_palette_fg_color = "#CDD6F5"
-
-  config.animation_fps = 1
+	config.animation_fps = 30
 end
 
 return module
