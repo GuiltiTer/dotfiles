ba73828f2495254007deb6bf8e4ca1b1966ff3c4
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2024-12-20 21:47:17 +0330

    Add nix configs

--- .config/zsh/modules/app_configs.zsh
+++ .config/zsh/modules/app_configs.zsh
@@ -32,3 +32,7 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 . /opt/homebrew/opt/asdf/libexec/asdf.sh
+
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
