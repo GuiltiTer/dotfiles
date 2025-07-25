1cc0829df286916e8858656a2a48aca8323b5ace
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-07-25 07:58:20 +0330

    Update yazi's config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,35 @@
 require("git"):setup()
+require("starship"):setup()
+require("yatline"):setup({
+	show_background = false,
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+	header_line = {},
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_path" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
