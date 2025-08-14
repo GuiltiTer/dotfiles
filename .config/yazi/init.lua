001169db4678f9b229f81967d79a64c5e9fce95c
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-08-15 00:37:06 +0330

    fix(yazi): show tabs in status line

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,35 +1,41 @@
 require("git"):setup()
-require("starship"):setup()
 require("yatline"):setup({
 	show_background = true,
 	section_separator = { open = "", close = "" },
 	part_separator = { open = "", close = "" },
 	inverse_separator = { open = "", close = "" },
-	header_line = {},
+	component_positions = { "header", "tab", "status" },
+	header_line = {
+		left = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+	},
 	status_line = {
 		left = {
 			section_a = {
 				{ type = "string", custom = false, name = "tab_mode" },
 			},
 			section_b = {
-				{ type = "string", custom = false, name = "hovered_size" },
+				{ type = "coloreds", custom = false, name = "count" },
 			},
 			section_c = {
-				{ type = "string", custom = false, name = "hovered_path" },
-				{ type = "coloreds", custom = false, name = "count" },
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
 			},
 		},
 		right = {
-			section_a = {
-				{ type = "string", custom = false, name = "cursor_position" },
-			},
+			section_a = {},
 			section_b = {
-				{ type = "string", custom = false, name = "cursor_percentage" },
-			},
-			section_c = {
-				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
 				{ type = "coloreds", custom = false, name = "permissions" },
+				{ type = "string", custom = false, name = "hovered_size" },
 			},
+			section_c = {},
 		},
 	},
 })
