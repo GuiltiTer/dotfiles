549ae7eacd7565c600ed05c8724e75a3d0ccc7dd
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-10-28 21:38:01 +0330

    fix(zsh): remove removed apps config

--- .config/zsh/modules/app_configs.zsh
+++ .config/zsh/modules/app_configs.zsh
@@ -8,25 +8,16 @@ lazyload ghcup ghci ghc cabal stack -- '[ -f "/Users/guiltiter/.ghcup/env" ] && 
 
 export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-lazyload pyenv -- 'eval "$(pyenv init -)"'
-
 eval "$(direnv hook zsh)"
 
 source <(fzf --zsh)
 
 lazyload conda -- 'conda_init'
 
-export PATH="$PATH:/Users/guiltiter/.local/bin" # pipx
-
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
 
 export PATH="$PATH:/Users/guiltiter/Library/Application Support/JetBrains/Toolbox/scripts"
-
-export MODULAR_HOME="$HOME/.modular"
-export PATH="$HOME/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
