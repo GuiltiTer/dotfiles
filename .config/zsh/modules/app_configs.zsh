975c62e5f531a466fb331716d38f257aca006744
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-11-02 16:22:06 +0330

    feat(zsh): add local bin to path

--- .config/zsh/modules/app_configs.zsh
+++ .config/zsh/modules/app_configs.zsh
@@ -31,3 +31,5 @@ fi
 export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
 zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
 source <(carapace _carapace)
+
+export PATH="/Users/guiltiter/.local/bin:$PATH"
