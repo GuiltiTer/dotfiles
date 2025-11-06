b9ff164ddd0eea86a7ec5e5cff7e04a65d20edd9
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-11-07 01:48:48 +0330

    feat(zsh): add mise

--- .config/zsh/modules/app_configs.zsh
+++ .config/zsh/modules/app_configs.zsh
@@ -1,35 +1,41 @@
+# homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+#mise
+eval "$(mise activate zsh)"
+
+#zoxide
 eval "$(zoxide init --cmd cd zsh)"
 
+# opam
 lazyload ocaml opam dune utop -- '[[ ! -r /Users/guiltiter/.opam/opam-init/init.zsh ]] || source /Users/guiltiter/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null'
 
+# ghcup
 lazyload ghcup ghci ghc cabal stack -- '[ -f "/Users/guiltiter/.ghcup/env" ] && source "/Users/guiltiter/.ghcup/env"'
 
+# curl
 export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
+# direnv
 eval "$(direnv hook zsh)"
 
+# fzf
 source <(fzf --zsh)
 
+# conda
 lazyload conda -- 'conda_init'
 
+# go
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
 
+# jetbrains toolbox
 export PATH="$PATH:/Users/guiltiter/Library/Application Support/JetBrains/Toolbox/scripts"
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-	. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-fi
-
+# carapce
 export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
 zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
 source <(carapace _carapace)
 
+# local bin
 export PATH="/Users/guiltiter/.local/bin:$PATH"
