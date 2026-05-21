af987e2393b139ae387aae02786ce5d3dfd480d3
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-05-21 21:22:48 +0330

    feat(zsh): add more proxy functions

--- .config/zsh/modules/functions.zsh
+++ .config/zsh/modules/functions.zsh
@@ -5,8 +5,38 @@ function lzyadm() {
 }
 
 function prox() {
-	local server=$(cat ~/.config/proxy_server.txt)
-	eval "HTTPS_PROXY='$server' HTTP_PROXY='$server' $@"
+    local server
+    server="$(<~/.config/proxy_server.txt)" || return
+
+    HTTP_PROXY="$server" \
+    HTTPS_PROXY="$server" \
+    ALL_PROXY="$server" \
+    http_proxy="$server" \
+    https_proxy="$server" \
+    all_proxy="$server" \
+    command "$@"
+}
+
+function prox_set() {
+    local server
+    server="$(<~/.config/proxy_server.txt)" || return
+
+    export HTTP_PROXY="$server"
+    export HTTPS_PROXY="$server"
+    export ALL_PROXY="$server"
+
+    export http_proxy="$server"
+    export https_proxy="$server"
+    export all_proxy="$server"
+
+    print "Proxy enabled: $server"
+}
+
+function prox_unset() {
+    unset HTTP_PROXY HTTPS_PROXY ALL_PROXY
+    unset http_proxy https_proxy all_proxy
+
+    print "Proxy disabled"
 }
 
 function yazi_keep_cwd() {
