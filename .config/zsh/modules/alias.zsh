ed74d536308f6c2685f3a262c3eeca55fb6079da
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-06-14 21:08:18 +0330

    feat(zsh): add suffix aliases

--- .config/zsh/modules/alias.zsh
+++ .config/zsh/modules/alias.zsh
@@ -13,10 +13,16 @@ alias cat=bat
 alias intel_login='env /usr/bin/arch -x86_64 /bin/zsh --login'
 
 alias packages='brew leaves | fzf'
-alias upgrade='topgrade --only brew_cask brew_formula custom_commands go pipx cargo self_update'
+alias upgrade='topgrade --only brew_cask brew_formula custom_commands yazi go pipx cargo self_update'
 alias gitignore='npx add-gitignore'
 alias icat='kitty +kitten icat'
 alias yaegi='rlwrap yaegi'
 
 alias os='query_to_opencode_server'
 alias om='select_opencode_model'
+
+
+alias -s json='jless'
+alias -s csv='csvlens'
+for e (zip rar dmg pdf) alias -s $e='openn'
+for e (py cpp c++ c h go rs lua toml txt md) alias -s $e='$EDITOR'
