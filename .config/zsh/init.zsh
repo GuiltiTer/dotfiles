f7316be6468285ffba3426f0fdf67cc19db02875
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-06-16 14:18:03 +0330

    fix(zsh): update configs

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -6,10 +6,11 @@ zinit wait lucid light-mode for Aloxaf/fzf-tab
 zinit wait lucid light-mode for zsh-users/zsh-history-substring-search
 zinit wait lucid light-mode for hlissner/zsh-autopair
 zinit wait lucid light-mode for jeffreytse/zsh-vi-mode
-zinit wait lucid light-mode multisrc'env.zsh alias.zsh functions.zsh keymaps.zsh opencode.zsh prox.zsh styles.zsh' for $ZSH_CONFIG/modules
+zinit wait lucid light-mode multisrc'env.zsh alias.zsh functions.zsh keymaps.zsh opencode.zsh prox.zsh' for $ZSH_CONFIG/modules
 
 zinit light-mode compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh' for sindresorhus/pure
 
+zinit light-mode pick'styles.zsh' for $ZSH_CONFIG/modules
 zinit blockf light-mode for zsh-users/zsh-completions
 autoload -Uz compinit && compinit
 zinit wait lucid light-mode for atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
--- .config/zsh/modules/styles.zsh
+++ .config/zsh/modules/styles.zsh
@@ -1,4 +1,3 @@
-zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:*' switch-group '<' '>'
