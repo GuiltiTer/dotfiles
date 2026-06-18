e97fbc7ea52ccc7a42013ff5e7f8899e0b5ecb85
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-06-19 01:23:52 +0330

    fix(zsh): remove zsh-vim-mode

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -3,9 +3,7 @@ source $ZSH_CONFIG/bootstrap.zsh
 
 zinit wait lucid light-mode for zimfw/environment
 zinit wait lucid light-mode for Aloxaf/fzf-tab
-zinit wait lucid light-mode for zsh-users/zsh-history-substring-search
 zinit wait lucid light-mode for hlissner/zsh-autopair
-zinit wait lucid light-mode for jeffreytse/zsh-vi-mode
 zinit wait lucid light-mode for Tarrasch/zsh-bd
 zinit wait lucid light-mode multisrc'env.zsh alias.zsh functions.zsh keymaps.zsh opencode.zsh prox.zsh' for $ZSH_CONFIG/modules
 
@@ -14,8 +12,8 @@ zinit light-mode compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh' for sin
 zinit light-mode pick'styles.zsh' for $ZSH_CONFIG/modules
 zinit blockf light-mode for zsh-users/zsh-completions
 autoload -Uz compinit && compinit
-zinit wait lucid light-mode for atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
-zinit wait lucid light-mode for zdharma-continuum/fast-syntax-highlighting
+zinit light-mode for atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
+zinit light-mode for zdharma-continuum/fast-syntax-highlighting
 
 zinit light qoomon/zsh-lazyload
 source $ZSH_CONFIG/modules/app_configs.zsh
