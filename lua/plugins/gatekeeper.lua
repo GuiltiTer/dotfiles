736c6bf57a2bcfc414e56fe81f9ef1c06873d52e
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-08-04 19:35:19 +0330

    feat(gatekeeper): add omp exclusion file name

--- lua/plugins/gatekeeper.lua
+++ lua/plugins/gatekeeper.lua
@@ -5,6 +5,7 @@ return {
     exclude_regex = {
       "zsh......%.zsh", -- zsh command line edit
       "prompt%.md$", -- allow prompt.md to be edited
+      "omp%-editor%-[0-9a-fA-F]+%.omp%.md$", -- allow Oh My Pi editor prompts outside the project
     },
   },
 }
