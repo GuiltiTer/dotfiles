7cea1a944db597cd42c71e102f7669c2965db628
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-09-07 18:43:06 +0330

    fix(zsh): update deja config

--- shell/.config/zsh/modules/app_configs.zsh
+++ shell/.config/zsh/modules/app_configs.zsh
@@ -28,7 +28,7 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # deja
 export DEJA_CYCLE_KEY='^[[Z'
-eval "$(deja init zsh)"
+[ -f "$HOME/.local/share/deja/init.zsh" ] && source $HOME/.local/share/deja/init.zsh
 
 # obsidian
 export PATH="$PATH:/Applications/Obsidian.app/Contents/MacOS"
