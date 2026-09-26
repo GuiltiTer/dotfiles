164a89bc3e95a5d0a53ff9f02666e30f602a2658
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2026-09-26 19:20:19 +0330

    fix(brew): move Brewfile to global brew dir

---
+++ brew/.config/homebrew/update_brewfile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+brew bundle dump --global --force --no-describe --no-tap --no-vscode
--- scripts/.config/update_brewfile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-brew bundle dump --force --no-describe --no-tap --file=$HOME/.config/Brewfile
